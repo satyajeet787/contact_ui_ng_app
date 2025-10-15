8353c2ddbc04b1cfbdc80d902a651e439e81d334
satyajeet787 <79631964+satyajeet787@users.noreply.github.com>
2025-10-15 21:27:38 +0530

    Update contact.service.ts

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -8,7 +8,7 @@ import { Contact } from './contact';
 })
 export class ContactService {
 
-  private baseUrl="http://ab5d88d3713894ac1a1177ce76b11877-1673811767.us-west-2.elb.amazonaws.com/"
+  private baseUrl="http://192.168.0.20/"
 
   constructor(private httpClient:HttpClient) { }
 
